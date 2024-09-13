f304a5bc3459061ac3fcc196a7242fc47dbd5758
netyc <netyc@MIKI>
2024-09-13 13:17:49 +0200

    new Game Controller

---
+++ Battleship_Royal.Api/Controllers/NewGameController.cs
@@ -0,0 +1,18 @@
+﻿using Battleship_Royal.Api.Requests;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Battleship_Royal.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewGameController(IMediator mediator) : ControllerBase
+    {
+        [HttpPost("new-game")]
+        public async Task<IActionResult> NewGame([FromBody] NewGameRequest request)
+        {
+            var result = await mediator.Send(request);
+            return Ok(result);
+        }
+    }
+}
