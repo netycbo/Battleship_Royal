466da3c6c5b66437a4bc688dbcb4f551c70a27ed
netyc <netyc@MIKI>
2024-12-04 12:48:56 +0100

    Added state managment

---
+++ Battleship_Royal/Components/Game Components/StateManagment/IStateContainer.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Battleship_Royal.Components.Game_Components
+{
+    public interface IStateContainer
+    {
+        int DifficultyLevel { get; set; }
+        string PlayerNickName { get; set; }
+        int Timer { get; set; }
+
+        event Action? OnStateChange;
+    }
+}
---
+++ Battleship_Royal/Components/Game Components/StateManagment/StateContainer.cs
@@ -0,0 +1,40 @@
+﻿namespace Battleship_Royal.Components.Game_Components
+{
+    public class StateContainer : IStateContainer
+    {
+        private string playerNickName;
+        private int difficultyLevel;
+        private int timer;
+        public string PlayerNickName
+        {
+            get => playerNickName;
+            set
+            {
+                playerNickName = value;
+                NotifyStateChanged();
+            }
+        }
+        public int DifficultyLevel
+        {
+            get => difficultyLevel;
+            set
+            {
+                difficultyLevel = value;
+                NotifyStateChanged();
+            }
+        }
+        public int Timer
+        {
+            get => timer;
+            set
+            {
+                timer = value;
+                NotifyStateChanged();
+            }
+        }
+
+        public event Action? OnStateChange;
+
+        private void NotifyStateChanged() => OnStateChange?.Invoke();
+    }
+}
